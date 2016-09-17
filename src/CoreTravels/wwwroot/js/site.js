14282523e934500ff9ffc99ca4d2390be0563aa1
Marlon Taborda Rodas <Marlon Taborda Rodas>
2016-09-17 17:56:38 -0500

    First commit core project

---
+++ src/CoreTravels/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿(function (){
+    //var txtUsername = $("#username");
+    //txtUsername.text("Maxwell54");
+
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.css("background-color", "#888");
+    //});
+    //main.on("mouseleave", function () {
+    //    main.css("background-color", "");;
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var menuItem = $(this);
+    //    menuItem.css("color", "red");
+    //})
+
+    var $sideBarContent = $("#sidebar,#content");
+
+    $("#sideBarToggle").on("click", function () {
+        $sideBarContent.toggleClass("hide-sidebar");
+    })    
+})();
